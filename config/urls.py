f921dc11015a8ebeb3297903c4a723f1114ad9da
SakariaNdadi <oipapi.ndadi@gmail.com>
2025-01-20 16:29:32 +0200

    code: refactored

--- config/urls.py
+++ config/urls.py
@@ -5,14 +5,15 @@ from django.urls import include, path
 
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
-    path("__reload__/", include("django_browser_reload.urls")),
     path("account/", include("apps.accounts.urls")),
     path("exam/", include("apps.exam.urls", namespace="exam")),
     path("", include("apps.pages.urls", namespace="pages")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if not settings.DEBUG:
     urlpatterns.append(path("not/admin/", admin.site.urls))
 else:
-    urlpatterns.append(path("admin/", admin.site.urls))
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns.append(
+        path("admin/", admin.site.urls),
+        path("__reload__/", include("django_browser_reload.urls")),
+    )
