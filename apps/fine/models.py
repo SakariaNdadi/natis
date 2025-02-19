cdf13cffc573f9d752d7b8123053e07cb2d055f6
Sakaria Ndadi <oipapi.ndadi@gmail.com>
2025-02-19 20:34:51 +0200

    code: added fine app models

--- apps/fine/models.py
+++ apps/fine/models.py
@@ -1,2 +1,13 @@
+from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField
+
+    def __str__(self):
+        return self.name
+
+class Fine(models.Model):
+    category = models.ForeignKey(Category, related_name="fines", on_delete=models.PROTECT)
+    code = models.PositiveIntegerField()
+    description = models.TextField()
+    fine = models.CharField(max_length=4)
