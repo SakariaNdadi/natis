bb90757d35c06179ce907761b40cbb65827b1c58
Sakaria Ndadi <oipapi.ndadi@gmail.com>
2025-02-20 08:20:48 +0200

    code: unique category name

--- apps/fine/models.py
+++ apps/fine/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, blank=True)
+    name = models.CharField(max_length=255, blank=True, unique=True)
 
     def __str__(self):
         return self.name
