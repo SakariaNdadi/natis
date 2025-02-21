0ac859ccf15c9dee6c1e33e2ec06425410f78520
Sakaria Ndadi <oipapi.ndadi@gmail.com>
2025-02-21 17:46:08 +0200

    code: capitalize category name

--- apps/fine/models.py
+++ apps/fine/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255, blank=True, unique=True)
 
     def __str__(self):
-        return self.name
-    
+        return self.name.capitalize()
+
     class Meta:
         verbose_name_plural = "categories"
 
+
 class Fine(models.Model):
-    category = models.ForeignKey(Category, related_name="fines", on_delete=models.PROTECT)
+    category = models.ForeignKey(
+        Category, related_name="fines", on_delete=models.PROTECT
+    )
     code = models.PositiveIntegerField()
     description = models.TextField()
     fine = models.CharField(max_length=4)
